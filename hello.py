885fdd5985b7225f5499babcacd9ecd7b4b7b31d
Andrew Han <andrewhan24@gmail.com>
2020-06-30 22:53:48 -0700

    long text pt 2

--- hello.py
+++ hello.py
@@ -20,7 +20,7 @@ def get_all_feed():
         },
         {
             "username": "user_b",
-            "text": "whats poppin?",
+            "text": "So I’m currently an 18 year old boy who’s been pro life for about as long as I’ve known abortion existed. I just graduated high school a few weeks ago and this is pretty much just a rant about the hive mind of my now former school. Being pro life in my generation is something that gets looked down on, and people’s reaction to someone saying they’re pro life is about equal to if you tell them you kill puppies for fun. That’s the level of shock and disgust people my age have for someone being pro life. Rational conversations don’t exist since holding that opinion makes you a misogynistic prick not worth their time. Anytime abortion is brought up they make the same few points and these points themselves prove they have zero knowledge of the science behind it. The points are",
             "created_at": int(time.time())+10,
         },
         {
