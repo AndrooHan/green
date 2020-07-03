f01a1484bd430483d222a8f8807f3c246367b049
Andrew Han <andrewhan24@gmail.com>
2020-07-03 11:20:49 -0700

    import request from flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 import json
 import time
 
@@ -14,7 +15,7 @@ feed_posts = [
     },
     {
         "username": "user_b",
-        "text": "So I’m currently an 18 year old boy who’s been pro life for about as long as I’ve known abortion existed. I just graduated high school a few weeks ago and this is pretty much just a rant about the hive mind of my now former school. Being pro life in my generation is something that gets looked down on, and people’s reaction to someone saying they’re pro life is about equal to if you tell them you kill puppies for fun. That’s the level of shock and disgust people my age have for someone being pro life. Rational conversations don’t exist since holding that opinion makes you a misogynistic prick not worth their time. Anytime abortion is brought up they make the same few points and these points themselves prove they have zero knowledge of the science behind it. The points are",
+        "text": "So Im currently an 18 year old boy whos been pro life for about as long as Ive known abortion existed. I just graduated high school a few weeks ago and this is pretty much just a rant about the hive mind of my now former school. Being pro life in my generation is something that gets looked down on, and peoples reaction to someone saying theyre pro life is about equal to if you tell them you kill puppies for fun. Thats the level of shock and disgust people my age have for someone being pro life. Rational conversations dont exist since holding that opinion makes you a misogynistic prick not worth their time. Anytime abortion is brought up they make the same few points and these points themselves prove they have zero knowledge of the science behind it. The points are",
         "created_at": int(time.time())+10,
     },
     {
