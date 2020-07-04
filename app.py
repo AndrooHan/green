7da653572b27a3e76a298daf14e6def7c89641f4
Andrew Han <andrewhan24@gmail.com>
2020-07-03 22:30:10 -0700

    add a fetch feed by lat long

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import render_template
 import json
 import time
 import random
 import string
+import geo
+import matplotlib.pyplot as plt
 
 
 app = Flask(__name__)
@@ -14,18 +17,36 @@ feed_posts = [
         "username": "user_a",
         "text": "first post!",
         "created_at": int(time.time()),
+        "latitude": 37.2310016,
+        "longitude": -121.7691648,
     },
     {
         "username": "user_b",
         "text": "So Im currently an 18 year old boy whos been pro life for about as long as Ive known abortion existed. I just graduated high school a few weeks ago and this is pretty much just a rant about the hive mind of my now former school. Being pro life in my generation is something that gets looked down on, and peoples reaction to someone saying theyre pro life is about equal to if you tell them you kill puppies for fun. Thats the level of shock and disgust people my age have for someone being pro life. Rational conversations dont exist since holding that opinion makes you a misogynistic prick not worth their time. Anytime abortion is brought up they make the same few points and these points themselves prove they have zero knowledge of the science behind it. The points are",
         "created_at": int(time.time())+10,
+        "latitude": 37.2310016,
+        "longitude": -121.7691648,
     },
     {
         "username": "user_c",
         "text": "hello everyone",
         "created_at": int(time.time())+20,
+        "latitude": 37.2310016,
+        "longitude": -121.7691648,
     },
 ]
+
+@app.route('/test')
+def random_test():
+    gs = geo.test()
+    
+    gs.plot()
+    plt.show()
+    # return render_template('simple.html',  tables=[gs.to_html(classes='data')], titles=gs.columns.values)
+    return "looool"
+    # gs.savefig('img/plot.png')
+    # return render_template('plot.html', url='/img/plot.png')
+    # return "cool"
 
 @app.route('/')
 def hello_world():
@@ -35,6 +56,22 @@ def hello_world():
 def get_all_feed():
     global feed_posts
     return jsonify([post for post in get_feed_posts()])
+
+
+#/api/feed?latitude=34.2323&longitude=-232.99222
+@app.route('/api/feed')
+def get_specific_feed():
+    global feed_posts
+
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+
+    print("latitude: " + latitude)
+    print("longitude: " + longitude)
+    circle = geo.find_circle(latitude, longitude, 10000)
+
+    # find posts within the circle
+    return jsonify([post for post in get_feed_posts() if geo.inside_polygon(circle, post['latitude'], post['longitude'])])
 
 @app.route('/add', methods=['POST'])
 def add_message():
---
+++ geo.py
@@ -0,0 +1,42 @@
+import geopandas as gpd
+from shapely.geometry import Polygon, Point
+import math
+
+def test():
+	states = gpd.read_file('us-states.json')
+	cali_multi_poly = states.loc[states['NAME'] == 'California'].geometry
+	return cali_multi_poly
+	# states.show()
+	# print(states.head())
+
+	# p1 = Point(36.527295,-120.418203)
+	# cali_multi_poly = states.loc[states['NAME'] == 'California'].geometry
+	# print("Cali")
+	# print(type(cali_multi_poly))
+	# print(cali_multi_poly)
+	# contain = cali_multi_poly.intersects(p1)
+	# print("Contains")
+	# print(contain)
+	# return states
+
+def find_circle(centerLat, centerLon, radius):
+	N = 10 # number of discrete sample points to be generated along the circle
+
+	# generate points
+	circlePoints = []
+	for k in xrange(N):
+	    # compute
+	    angle = math.pi*2*k/N
+	    dx = radius*math.cos(angle)
+	    dy = radius*math.sin(angle)
+	    point = {}
+	    point['lat']= float(centerLat) + (180/math.pi)*(dy/6378137)
+	    point['lon']= float(centerLon) + (180/math.pi)*(dx/6378137)/math.cos(float(centerLat)*math.pi/180)
+	    # add to list
+	    circlePoints.append((point['lat'], point['lon']))
+
+	return Polygon(circlePoints)
+
+def inside_polygon(polygon, latitude, longitude):
+	p = Point(latitude, longitude)
+	return polygon.contains(p)
