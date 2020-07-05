696d098a1a06efbe42ca85b31a01f0d8bb426014
Andrew Han <andrewhan24@gmail.com>
2020-07-04 22:44:59 -0700

    add redis and refresh

--- app.py
+++ app.py
@@ -7,33 +7,19 @@ import time
 import random
 import string
 import geo
+import uuid
+import redis
+import os
+import fakeredis
+import socket
+
 
 
 app = Flask(__name__)
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
 
-feed_posts = [
-    {
-        "username": "user_a",
-        "text": "first post!",
-        "created_at": int(time.time()),
-        "latitude": 37.2310016,
-        "longitude": -121.7691648,
-    },
-    {
-        "username": "user_b",
-        "text": "So Im currently an 18 year old boy whos been pro life for about as long as Ive known abortion existed. I just graduated high school a few weeks ago and this is pretty much just a rant about the hive mind of my now former school. Being pro life in my generation is something that gets looked down on, and peoples reaction to someone saying theyre pro life is about equal to if you tell them you kill puppies for fun. Thats the level of shock and disgust people my age have for someone being pro life. Rational conversations dont exist since holding that opinion makes you a misogynistic prick not worth their time. Anytime abortion is brought up they make the same few points and these points themselves prove they have zero knowledge of the science behind it. The points are",
-        "created_at": int(time.time())+10,
-        "latitude": 37.2310016,
-        "longitude": -121.7691648,
-    },
-    {
-        "username": "user_c",
-        "text": "hello everyone",
-        "created_at": int(time.time())+20,
-        "latitude": 37.2310016,
-        "longitude": -121.7691648,
-    },
-]
+r = fakeredis.FakeStrictRedis() if str(ip_address) == "127.0.0.1" else redis.from_url(os.environ.get("REDIS_URL"))
 
 @app.route('/test')
 def random_test():
@@ -59,35 +45,34 @@ def get_specific_feed():
 
 @app.route('/add', methods=['POST'])
 def add_message():
-    global feed_posts
     content = request.json
     latitude = content['latitude']
     longitude = content['longitude']
 
     print("latitude: " + str(latitude))
     print("longitude: " + str(longitude))
-    feed_posts.append(
-        {
-            "username": content['username'],
-            "text": content['text'],
-            "created_at": int(time.time()),
-            "latitude": latitude,
-            "longitude": longitude,
-        }
-    )
+    post = {
+        "username": content['username'],
+        "text": content['text'],
+        "created_at": int(time.time()),
+        "latitude": latitude,
+        "longitude": longitude,
+    }
+    add_to_redis(post)
+    
     return jsonify(get_feed_posts_within(latitude, longitude, 1))
 
-@app.route('/add-test')
-def add_message_test():
-    global feed_posts
-    feed_posts.append(
-        {
-            "username": ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)),
-            "text": ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(200)),
-            "created_at": int(time.time()),
-        }
-    )
-    return jsonify([post for post in get_feed_posts()])
+# @app.route('/add-test')
+# def add_message_test():
+#     global feed_posts
+#     feed_posts.append(
+#         {
+#             "username": ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)),
+#             "text": ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(200)),
+#             "created_at": int(time.time()),
+#         }
+#     )
+#     return jsonify([post for post in get_feed_posts()])
 
 def myFunc(post):
   return post['created_at']
@@ -111,9 +96,49 @@ def get_feed_posts_within(latitude, longitude, radius):
     return filtered_posts
 
 def get_feed_posts():
-    global feed_posts
+    keys = r.keys()
+    vals = r.mget(keys)
+    feed_posts = [json.loads(v) for v in vals]
     feed_posts.sort(reverse=True, key=myFunc)
     return feed_posts
+
+
+# Redis functions ===============================
+def seed_redis():
+    feed_posts = [
+        {
+            "username": "user_a",
+            "text": "first post!",
+            "created_at": int(time.time()),
+            "latitude": 37.2310016,
+            "longitude": -121.7691648,
+        },
+        {
+            "username": "user_b",
+            "text": "So Im currently an 18 year old boy whos been pro life for about as long as Ive known abortion existed. I just graduated high school a few weeks ago and this is pretty much just a rant about the hive mind of my now former school. Being pro life in my generation is something that gets looked down on, and peoples reaction to someone saying theyre pro life is about equal to if you tell them you kill puppies for fun. Thats the level of shock and disgust people my age have for someone being pro life. Rational conversations dont exist since holding that opinion makes you a misogynistic prick not worth their time. Anytime abortion is brought up they make the same few points and these points themselves prove they have zero knowledge of the science behind it. The points are",
+            "created_at": int(time.time())+10,
+            "latitude": 37.2310016,
+            "longitude": -121.7691648,
+        },
+        {
+            "username": "user_c",
+            "text": "hello everyone",
+            "created_at": int(time.time())+20,
+            "latitude": 37.2310016,
+            "longitude": -121.7691648,
+        },
+    ]
+    for post in feed_posts:  
+        json_post = json.dumps(post)
+        r.set(str(uuid.uuid4()), json_post)
+
+def add_to_redis(post):
+    r.set(str(uuid.uuid4()), post)
+
+
+
+seed_redis()
+
 
 
 
